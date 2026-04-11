e99f456926f6e81d6a9b84529ba1e795f9d35656
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-11 20:02:42 +0530

    Evaluated the Second Case -> Results were correct

--- model-registry/inference.py
+++ model-registry/inference.py
@@ -7,14 +7,14 @@ mlflow.set_tracking_uri("https://dagshub.com/AvanindraBose/MlFlow-Learning.mlflo
 dagshub.init(repo_owner='AvanindraBose', repo_name='MlFlow-Learning', mlflow=True)
 
 data = pd.DataFrame({
-    'Pregnancies': [1],
-    'Glucose': [85],
-    'BloodPressure': [66],
-    'SkinThickness': [29],
+    'Pregnancies': [6],
+    'Glucose': [148],
+    'BloodPressure': [72],
+    'SkinThickness': [35],
     'Insulin': [0],
-    'BMI': [26.6],
-    'DiabetesPedigreeFunction': [0.351],
-    'Age': [31]
+    'BMI': [33.6],
+    'DiabetesPedigreeFunction': [0.627],
+    'Age': [50]
 })
 
 model_name = "diabities-rf"
