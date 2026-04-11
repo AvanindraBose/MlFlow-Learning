bf9cb6f08c841d45ae514cfb2adc45288476090b
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-11 20:01:46 +0530

    Evaluted the Test Case -> Results were correct

--- model-registry/inference.py
+++ model-registry/inference.py
@@ -1,16 +1,27 @@
 import mlflow.pyfunc
 import mlflow
-import numpy as np
+import pandas as pd
 import dagshub
 
 mlflow.set_tracking_uri("https://dagshub.com/AvanindraBose/MlFlow-Learning.mlflow")
 dagshub.init(repo_owner='AvanindraBose', repo_name='MlFlow-Learning', mlflow=True)
 
-data = np.array([1,85,66,29,0,26.6,0.351,31]).reshape(1,-1)
+data = pd.DataFrame({
+    'Pregnancies': [1],
+    'Glucose': [85],
+    'BloodPressure': [66],
+    'SkinThickness': [29],
+    'Insulin': [0],
+    'BMI': [26.6],
+    'DiabetesPedigreeFunction': [0.351],
+    'Age': [31]
+})
 
 model_name = "diabities-rf"
 model_version = 3
 
 model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{model_version}")
 
-print(model.predict(data))
+result = model.predict(data)
+print(f"Prediction: {result}")
+print(f"0 = No Diabetes, 1 = Diabetes")
